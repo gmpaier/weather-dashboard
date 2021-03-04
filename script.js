6d952e0aaff42b17fe22802be3404697d52a4d8f
grif <gmpaier@loyola.edu>
2021-03-03 23:18:50 -0500

    Program is pretty much finished. All city data are stored in an object array, which is saved to local storage. if the day is the same as the stored data, it will pull from the object array as opposed to making a new API call. also loads stored data if the same name is re-searched.

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-function Weather(name, date, cond, icon, temp, hum, wind, lat, lon, uvi, day1, day2, day3, day4, day5){
+function Weather(name, date, icon, temp, hum, wind, lat, lon, uvi, day1, day2, day3, day4, day5){
   this.name = name;
   this.date = date;
-  this.cond = cond;
   this.icon = icon;
   this.temp = temp;
   this.hum = hum;
@@ -9,17 +8,16 @@ function Weather(name, date, cond, icon, temp, hum, wind, lat, lon, uvi, day1, d
   this.lat = lat;
   this.lon = lon;
   this.uvi = uvi;
-  this.day1 = day1;
-  this.day2 = day2;
-  this.day3 = day3;
-  this.day4 = day4;
-  this.day5 = day5;   
+  this.day1 = {date: day1.date, icon: day1.icon, temp: day1.temp, hum: day1.hum};
+  this.day2 = {date: day2.date, icon: day2.icon, temp: day2.temp, hum: day2.hum};
+  this.day3 = {date: day3.date, icon: day3.icon, temp: day3.temp, hum: day3.hum};
+  this.day4 = {date: day4.date, icon: day4.icon, temp: day4.temp, hum: day4.hum};
+  this.day5 = {date: day5.date, icon: day5.icon, temp: day5.temp, hum: day5.hum};
 }
 
 var tempData = {
   name: "",
   date: "",
-  cond: "",
   icon: "",
   temp: "",
   hum: "",
@@ -59,15 +57,54 @@ var tempData = {
   } 
 };
 
+let today = Date.now();
 let cityList = [];
 
 
-
-function getAPI(){
-  const key = "75f04fe88f0718f0439006e20e72ddf1";
-  $("#display").show();
+function searchCity(){
   let city = $("#city-input").val();
   city = city.trim();
+  let upper = city.toUpperCase()
+  city = upper.charAt(0) + city.slice(1);
+  let boo = false;
+  let key = 0;
+  for (let i = 0; i < cityList.length; i++){
+    if (city === cityList[i].name){
+      boo = true;
+      key = i;
+    }
+  }
+  if (boo === false){
+    getAPI(city);
+  }
+  else {
+    let now = displayDate(today)
+    let then = displayDate(cityList[key].date)
+    if (now === then){
+      $("#display").show();
+      fill(cityList[key]);
+    }
+    else {
+      getAPI(city);
+    }
+  }
+}
+
+function clickCity(){
+  let key = parseInt(this.value);
+  let now = displayDate(today)
+  let then = displayDate(cityList[key].date)
+  if (now === then){
+    $("#display").show();
+    fill(cityList[key]);
+  }
+  else {
+    getAPI(cityList[key].name);
+  }
+}
+
+function getAPI(city){
+  const key = "75f04fe88f0718f0439006e20e72ddf1";
   var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
 
     fetch(requestUrl)
@@ -78,10 +115,9 @@ function getAPI(){
         return response.json();
       })
       .then(function (data) {
-          console.log(data);
+          $("#display").show();
           tempData.name = data.name;
-          tempData.date = new Date(parseInt(data.dt)*1000);
-          tempData.cond = data.weather[0].main;
+          tempData.date = parseInt(data.dt)*1000;
           tempData.icon = data.weather[0].icon;
           tempData.temp = data.main.temp;
           tempData.hum = data.main.humidity;
@@ -99,14 +135,16 @@ function getAPI(){
             .then(function (data) {
                 for (let i = 1; i < 6; i++){
                     let day = "day" + i;
-                    tempData[day].date = new Date(parseInt(data.daily[i].dt)*1000);
+                    tempData[day].date = parseInt(data.daily[i].dt*1000);
                     tempData[day].icon = data.daily[i].weather[0].icon;
                     tempData[day].temp = data.daily[i].temp.day;
                     tempData[day].hum = data.daily[i].humidity;
                 }
                 tempData.uvi = data.current.uvi;
                 fill(tempData);
-                cityList[cityList.length] = new Weather(tempData.name, tempData.date, tempData.cond, tempData.icon, tempData.temp, tempData.hum, tempData.wind, tempData.lat, tempData.lon, tempData.uvi, tempData.day1, tempData.day2, tempData.day3, tempData.day4, tempData.day5);
+                addCity(tempData, cityList.length);
+                cityList[cityList.length] = new Weather(tempData.name, tempData.date, tempData.icon, tempData.temp, tempData.hum, tempData.wind, tempData.lat, tempData.lon, tempData.uvi, tempData.day1, tempData.day2, tempData.day3, tempData.day4, tempData.day5);
+                localStorage.setItem("weatherData", JSON.stringify(cityList));
                 return data;
             })
             .catch(function (error) {
@@ -117,7 +155,23 @@ function getAPI(){
         console.error('There has been a problem with your fetch operation:', error);
       })
   
-}   
+}
+
+function colorUvi(uvi){
+  if (uvi < 3){
+    $("#uvi").css("background-color", "green");
+  }
+  else if (uvi >= 7){
+    $("#uvi").css("background-color", "red");
+  }
+  else {
+    $("#uvi").css("background-color", "gold");
+  }
+}
+
+function addCity(city, val){
+  $(".list-group").append('<button type="button" class="list-group-item list-group-item-action short" value="'+ val + '">' + city.name + '</button>');
+}
 
 function fill(city){
   $("#city-name").text(city.name);
@@ -127,6 +181,7 @@ function fill(city){
   $("#humidity").text(city.hum);
   $("#wind-speed").text(city.wind);
   $("#uvi").text(city.uvi);
+  colorUvi(city.uvi);
   for (let i = 1; i < 6; i++){
     let day = "day" + i;
     $("#date"+i).text(displayDate(city[day].date));
@@ -136,11 +191,32 @@ function fill(city){
   }
 }
 
-function displayDate (dt){
+function displayDate (utc){
+  let dt = new Date(utc);
   let format = (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear();
   return format;
 }
 
+function loadData(){
+  if(localStorage.getItem("weatherData") === null){}
+  else{
+    cityList = JSON.parse(localStorage.getItem("weatherData"));
+    for (let i = 0; i < cityList.length; i++){
+      addCity(cityList[i], i);
+    }
+  }
+  
+}
+
+function clearData(){
+  $(".list-group").empty();
+  localStorage.removeItem("weatherData");
+  cityList.splice(0, cityList.length);
+}
+
 //Runtime
+loadData();
 $("#display").hide();
-$("#search-btn").on("click", getAPI);
+$("#search-btn").on("click", searchCity);
+$("#clear-btn").on("click", clearData);
+$(document).on("click", ".list-group-item", clickCity);
